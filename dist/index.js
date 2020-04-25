cfd20b353367d2d309f372e8edaeff709e412aa2
samstr <samstr@gmail.com>
2020-04-25 19:35:36 +0100

    different import

--- dist/index.js
+++ dist/index.js
@@ -2,9 +2,6 @@
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
-var index = require('./sub/index.js');
-
 var hey = 'hi';
 
-exports.subthing = index.subthing;
 exports.hey = hey;
--- sub.js
+++ sub.js
@@ -1,1 +1,1 @@
-module.exports = require('./dist/sub/index.js');
+module.exports = require('./dist/sub');
